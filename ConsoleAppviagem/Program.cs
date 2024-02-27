4560823c9775dc8e5a83733bd77519345279644a
Usuario <Usuario@CPD02-M13>
2024-02-27 11:22:25 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleAppviagem/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppviagem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Qual a distancia precorrida na viagem");
+            double d = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual o valor do litro de conbustivel");
+            double pl = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual o consumo de seu veiculo");
+            double comsumo = double.Parse(Console.ReadLine());
+
+            double resultado1 = d / comsumo;
+
+            double resultado2 = pl * resultado1;
+
+
+
+            Console.WriteLine("o gato total com a viagem foi de:" + resultado2);
+            Console.ReadKey();
+
+
+        }
+    }
+}
